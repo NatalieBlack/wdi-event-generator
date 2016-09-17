775fd2c78881959d92bc0da28b55adf8e1dd04f3
NatalieBlack <natalievictoriablack@gmail.com>
2016-09-16 21:14:36 -0400

    initial commit

---
+++ cohort.rb
@@ -0,0 +1,115 @@
+require 'date'
+require 'active_support'
+require 'active_support/core_ext'
+require 'holidays'
+
+
+class Cohort
+#start day is calendar day
+#end day is calendar day
+#given week and day return calendar day
+#generate lecture days
+
+
+  def initialize(first_day)
+    @first_day = first_day
+  end
+
+  def last_day
+    @first_day + 8.weeks + 4.days
+  end
+
+  def no_lecture_on(day)
+    day.saturday? || day.sunday? || double_check_holiday(day)
+  end
+
+  def double_check_holiday(day)
+    potential_holidays = Holidays.on(day, :ca_on)
+    
+    if potential_holidays.any?
+      potential_holidays.each do |h|
+        print "Are you taking #{h} off? y/N: "
+        answer = gets.chomp
+        if answer.downcase == 'y'
+          return true
+        end
+      end
+    end
+
+    return false
+  end
+
+  def lectures
+    list = []
+    index = 0
+    (@first_day..last_day).each do |day|
+      unless no_lecture_on(day) 
+        list << Lecture.new(index, day)
+        index += 1
+      end
+    end
+
+    return list
+  end
+
+end
+
+class Lecture
+
+  LECTURE_TITLES = [
+    "OS and Git Fundamentals", 
+    "Ruby Fundamentals part 1", 
+    "Ruby Fundamentals part 2", 
+    "Object-Oriented Programming part 1", 
+    "Object-Oriented Programming part 2", 
+    "How To Think Like a Developer", 
+    "Debugging & Testing in Ruby", 
+    "HTTP Requests and Responses", 
+    "CSS Box Model & Templates", 
+    "HTML Forms & Submitting Data", 
+    "Saving Your Data/Intro To Databases", 
+    "Intro to Rails", 
+    "Routing & Controllers", 
+    "Views & Forms",
+    "SQL & ActiveRecord & Validations",
+    "Validations and Associations in Rails",
+    "Sessions & Authentication",
+    "Planning and Thinking Through a Rails Application",
+    "Teamwork with Git",
+    "Testing With Rails",
+    "Serving Different Representations of Data",
+    "Displays & Floats in CSS",
+    "Positioning & Intro to Sass",
+    "JavaScript Fundamentals",
+    "Intro to jQuery & DOM",
+    "Events, Callbacks & Closures",
+    "Client-side Applications",
+    "Intro to AJAX",
+    "AJAX & JSON",
+    "Forms & AJAX",
+    "Client-side Templating",
+    "Crowdfunder, Multi-Model Forms",
+    "Authentication & Authorization",
+    "Environments and Sending Email",
+    "Ruby Fundamentals Review",
+    "Intro to Responsive Web Design",
+    "Working with APIs",
+    "Scheduled Tasks and Background Jobs",
+    "Surprise 1",
+    "Surprise 2",
+    "Surprise 3",
+    "Surprise 4",
+    "Surprise 5",
+    "Preparing for Interviews",
+    "Deployment, Capistrano & Metrics"
+  ]
+
+  attr_reader :title, :date, :number
+
+  def initialize(number, date)
+    @number = number
+    @date = date
+    @title = LECTURE_TITLES[number]
+  end
+
+end
---
+++ generate.rb
@@ -0,0 +1,59 @@
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require_relative 'cohort'
+
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'WDI Event Generator'
+CLIENT_SECRETS_PATH = 'client_id.json'
+CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+                             "calendar-ruby-quickstart.yaml")
+SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR
+
+
+##
+# Ensure valid credentials, either by restoring from the saved credentials
+# files or intitiating an OAuth2 authorization. If authorization is required,
+# the user's default browser will be launched to approve the request.
+#
+# @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+def authorize
+  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+  client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(
+    client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(
+      base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the " +
+         "resulting code after authorization"
+    puts url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
+  end
+  credentials
+end
+
+# Initialize the API
+service = Google::Apis::CalendarV3::CalendarService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+
+calendar_id = 'bitmakerlabs.com_i2oth2018vdn6rlu96215j76ms@group.calendar.google.com'
+cohort = Cohort.new(Date.new(2016,10,17))
+cohort.lectures.each do |lecture|
+  d = lecture.date
+  tzoffset = (d.to_time.utc_offset/60/60).to_s
+  st_dt = DateTime.new(d.year, d.month, d.day,10,0,0,tzoffset)
+  ft_dt = DateTime.new(d.year, d.month, d.day,12,0,0,tzoffset)
+  st = Google::Apis::CalendarV3::EventDateTime.new(date_time: st_dt.rfc3339)
+  fn = Google::Apis::CalendarV3::EventDateTime.new(date_time: ft_dt.rfc3339)
+  event = Google::Apis::CalendarV3::Event.new(start: st, end: fn, summary: "Lesson #{lecture.number + 1}: #{lecture.title}")
+  service.insert_event(calendar_id, event)
+end
